e8eb9cd9d9c60cd9efe479c9e6349ae1b5f6cf51
alexandronischenko <alexandr.onischenko.2003@gmail.com>
2022-03-01 18:37:35 +0300

    Created project, added Podfile

---
+++ BigDeal/BigDealTests/BigDealTests.swift
@@ -0,0 +1,33 @@
+//
+//  BigDealTests.swift
+//  BigDealTests
+//
+//  Created by Alexandr Onischenko on 01.03.2022.
+//
+
+import XCTest
+@testable import BigDeal
+
+class BigDealTests: XCTestCase {
+
+    override func setUpWithError() throws {
+        // Put setup code here. This method is called before the invocation of each test method in the class.
+    }
+
+    override func tearDownWithError() throws {
+        // Put teardown code here. This method is called after the invocation of each test method in the class.
+    }
+
+    func testExample() throws {
+        // This is an example of a functional test case.
+        // Use XCTAssert and related functions to verify your tests produce the correct results.
+    }
+
+    func testPerformanceExample() throws {
+        // This is an example of a performance test case.
+        self.measure {
+            // Put the code you want to measure the time of here.
+        }
+    }
+
+}

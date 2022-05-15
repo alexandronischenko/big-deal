79e6128d29ecf1dbc5bac488d8cfdc012f3217c5
Murtazin <renatmurtazin@yahoo.com>
2022-05-15 20:33:11 +0300

    profile flow screens module builders were changed.

--- BigDeal/BigDeal/Flow/Profile/Main/ModuleBuilder/ProfileMainModuleBuilder.swift
+++ BigDeal/BigDeal/Flow/Profile/Main/ModuleBuilder/ProfileMainModuleBuilder.swift
@@ -2,8 +2,19 @@ import Foundation
 import UIKit
 
 class ProfileMainModuleBuilder {
-    func buildModule(coordinator: ProfileBaseCoordinatorProtocol) -> UIViewController {
-        let presenter = ProfileMainPresenter(coordinator: coordinator)
+    // MARK: - Properties
+    
+    var coordinator: ProfileBaseCoordinatorProtocol
+    
+    // MARK: - Initializers
+    
+    init(coordinator: ProfileBaseCoordinatorProtocol) {
+        self.coordinator = coordinator
+    }
+    // MARK: - Functions
+    
+    func buildModule() -> UIViewController {
+        let presenter = ProfileMainPresenter(coordinator: self.coordinator)
         let viewController = ProfileMainViewController(output: presenter)
         presenter.input = viewController
         return viewController
